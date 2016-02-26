17ea4fae69545fe1ad8305f258e7a7dd638f368b
iraghib <iraghib@gmail.com>
2016-02-26 16:57:17 +0100

    Changes By Naveen

---
+++ src/com/yoox/commerce/security/commands/YooxDBAuthenticationCmdImpl.java
@@ -0,0 +1,34 @@
+
+
+package com.yoox.commerce.security.commands;
+
+import com.ibm.commerce.exception.ECException;
+import com.ibm.commerce.security.commands.DBAuthenticationCmd;
+import com.ibm.commerce.security.commands.DBAuthenticationCmdImpl;
+
+
+
+public class YooxDBAuthenticationCmdImpl extends DBAuthenticationCmdImpl    
+implements DBAuthenticationCmd
+{
+
+            public YooxDBAuthenticationCmdImpl()
+            {  
+            	super();            	
+            	
+            }
+            
+                   
+
+            public void performExecute()
+                throws ECException
+            {
+            	System.err.println("\n CUSTOM Project Memeber Logic Execution Started");            	
+            	super.performExecute();
+            	
+            	
+            }
+
+}
+
+
